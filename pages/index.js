642ac35e8830d1320de84bacc78936a606f4ee8a
Arifudin Myf <62134968+Arifudinmyf@users.noreply.github.com>
2022-12-07 13:44:45 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Slider from './slider'
 
 export default function Home() {
   return (
@@ -12,6 +13,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <div>
+        <Slider />
+        </div>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
